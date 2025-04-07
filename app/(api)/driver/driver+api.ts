f9fa8e8b0cd54b8f520da718deb0bfbfe77aea58
igorms-pro <igorms.pro@gmail.com>
2025-04-07 10:07:29 +0200

    db driver - ride ok - min driver not working

---
+++ app/(api)/driver/driver+api.ts
@@ -0,0 +1,16 @@
+import { neon } from "@neondatabase/serverless";
+
+export async function GET(request: Request) {
+  try {
+    const sql = neon(`${process.env.EXPO_PUBLIC_DATABASE_URL}`);
+
+    const response = await sql`SELECT * FROM drivers`;
+
+    console.log("SQL", JSON.stringify(response));
+
+    return Response.json({ data: response });
+  } catch (error) {
+    console.error("Error fetching drivers:", error);
+    return Response.json({ error: "Internal Server Error" }, { status: 500 });
+  }
+}
